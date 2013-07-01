876428d61a90c011496927d206fbf48f65860841
Matthew MacManes <macmanes@gmail.com>
2013-07-01 08:07:18 -0700

    add $(RUN)

--- social.mk
+++ social.mk
@@ -1,7 +1,19 @@
 #!/usr/bin/make -s
 
+###########################################
+###        -usage 'make -sf social.mk READ1=/location/or/read1.fastq READ2=/location/of/read2.fastq'
+###        
+###
+###
+###
+###
+###
+###         -Make sure your Trinity base directory 
+###         	is set properly
+###         -Make sure barcode file is located with
+###           BCODES= tag
+############################################
 TRINITY := /home/macmanes/trinityrnaseq_r2013-02-25
-MUS := /media/macmanes/hd/flux/genomes/mus/Mus_musculus.GRCm38.71.cdna.all.fa
 BCODES := /home/macmanes/Dropbox
 
 
@@ -40,25 +52,25 @@ trim: $(READ1) $(READ2)
 		rm T.$$TRIM.pp.2.fq T.$$TRIM.up.2.fq T.$$TRIM.pp.1.fq T.$$TRIM.up.1.fq ; \
 	done
 
-correct: hippo_left.$$TRIM.fastq hippo_right.$$TRIM.fastq
+correct: $(RUN)_left.$$TRIM.fastq $(RUN)_right.$$TRIM.fastq
 	@echo About to start error correction
 	perl fastq-converter-v2.0.pl ./ ./ 1 #files MUST have fastq extension
-	sed -i 's_[0-9]$_&/1_' hippo_left.fa #add /1 to ID reads as left
-	sed -i 's_[0-9]$_&/2_' hippo_right.fa #add /2 to ID reads as right
-	sed -i 's_^>.*[0-9]$_&/1_' hippo_left.q
-	sed -i 's_^>.*[0-9]$_&/2_' hippo_right.q
-	cat hippo_left.fa hippo_right.fa > both.fa
-	cat hippo_left.q hippo_right.q > both.q
+	sed -i 's_[0-9]$_&/1_' $(RUN)_left.fa #add /1 to ID reads as left
+	sed -i 's_[0-9]$_&/2_' $(RUN)_right.fa #add /2 to ID reads as right
+	sed -i 's_^>.*[0-9]$_&/1_' $(RUN)_left.q
+	sed -i 's_^>.*[0-9]$_&/2_' $(RUN)_right.q
+	cat $(RUN)_left.fa hippo_right.fa > both.fa
+	cat $(RUN)_left.q $(RUN)_right.q > both.q
 	$(REPTILE)/reptile-omp config.analy #Do error corection
 	$(REPTILE)/utils/reptile_merger/reptile_merger both.fa both.reptile.err both.reptile.corr.fa #make error corrected fasta file
-	grep -aA1 '/1' both.reptile.corr.fa > hippo.left.rept.corr.fa
-	grep -aA1 '/2' both.reptile.corr.fa > hippo.right.rept.corr.fa
+	grep -aA1 '/1' both.reptile.corr.fa > $(RUN).left.rept.corr.fa
+	grep -aA1 '/2' both.reptile.corr.fa > $(RUN).right.rept.corr.fa
 
-assemble:  hippo.left.rept.corr.fa hippo.right.rept.corr.fa
+assemble:  $(RUN).left.rept.corr.fa $(RUN).right.rept.corr.fa
 	$(TRINITY)/Trinity.pl --full_cleanup --SS_lib_type RF --min_kmer_cov 2 --seqType fa --JM 30G \
-	--left hippo.left.rept.corr.fa --right hippo.right.rept.corr.fa --CPU 8 --output hippo
+	--left $(RUN).left.rept.corr.fa --right $(RUN).right.rept.corr.fa --CPU 8 --output $(RUN)
 	
-rsem: hippo.Trinity.fasta
+rsem: $(RUN).Trinity.fasta
 	$(TRINITY)/util/RSEM_util/run_RSEM_align_n_estimate.pl --transcripts $< --seqType fq --left $(READ1) \
 	--right $(READ2) --thread_count 8 --SS_lib_type RF -- --bowtie-chunkmbs 512
 
