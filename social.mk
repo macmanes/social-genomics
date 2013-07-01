44e23fcda0c1cec8c1899fb1b49fbd999a08decf
Matthew MacManes <macmanes@gmail.com>
2013-07-01 08:15:18 -0700

    pulled merge out to its own step

--- social.mk
+++ social.mk
@@ -14,15 +14,15 @@
 ###           BCODES= tag
 ############################################
 TRINITY := /home/macmanes/trinityrnaseq_r2013-02-25
-BCODES := /home/macmanes/Dropbox
-
+BCODES := /home/macmanes/Dropbox/barcodes.fa
+CONFIG:= /home/macmanes/Dropbox/config.analy
 
 RUN=run
 READ1=left.fastq
 READ2=right.fastq
 TRIMMOMATIC ?= $(shell which 'trimmomatic-0.30.jar')
 
-all: check trim correct assemble rsem
+all: check trim correct merge assemble rsem
 
 check:
 	@echo "\n\n\n"###I am checking to see if you have all the dependancies installed.### "\n"
@@ -45,7 +45,7 @@ trim: $(READ1) $(READ2)
 		T.$$TRIM.up.1.fq \
 		T.$$TRIM.pp.2.fq \
 		T.$$TRIM.up.2.fq \
-		ILLUMINACLIP:$(BCODES)/barcodes.fa:2:40:15 \
+		ILLUMINACLIP:$(BCODES):2:40:15 \
 		LEADING:$$TRIM TRAILING:$$TRIM SLIDINGWINDOW:4:$$TRIM MINLEN:25 ; \
 		cat T.$$TRIM.pp.1.fq T.$$TRIM.up.1.fq > $(RUN)_left.$$TRIM.fastq ; \
 		cat T.$$TRIM.pp.2.fq T.$$TRIM.up.2.fq > $(RUN)_right.$$TRIM.fastq ; \
@@ -59,12 +59,15 @@ correct: $(RUN)_left.$$TRIM.fastq $(RUN)_right.$$TRIM.fastq
 	sed -i 's_[0-9]$_&/2_' $(RUN)_right.fa #add /2 to ID reads as right
 	sed -i 's_^>.*[0-9]$_&/1_' $(RUN)_left.q
 	sed -i 's_^>.*[0-9]$_&/2_' $(RUN)_right.q
-	cat $(RUN)_left.fa hippo_right.fa > both.fa
+	cat $(RUN)_left.fa $(RUN)_right.fa > both.fa
 	cat $(RUN)_left.q $(RUN)_right.q > both.q
-	$(REPTILE)/reptile-omp config.analy #Do error corection
-	$(REPTILE)/utils/reptile_merger/reptile_merger both.fa both.reptile.err both.reptile.corr.fa #make error corrected fasta file
+	reptile-omp $(CONFIG) #Do error corection
+
+merge: both.reptile.err
+	reptile_merger both.fa $< both.reptile.corr.fa #make error corrected fasta file
 	grep -aA1 '/1' both.reptile.corr.fa > $(RUN).left.rept.corr.fa
 	grep -aA1 '/2' both.reptile.corr.fa > $(RUN).right.rept.corr.fa
+	
 
 assemble:  $(RUN).left.rept.corr.fa $(RUN).right.rept.corr.fa
 	$(TRINITY)/Trinity.pl --full_cleanup --SS_lib_type RF --min_kmer_cov 2 --seqType fa --JM 30G \
